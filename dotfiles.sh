69d41d9704a127058a7c95a15acb3b333e69b092
Arvonit <me@arvind.dev>
2021-01-19 10:40:22 -0500

    Remove vscode since settings sync can be used instead

--- dotfiles.sh
+++ dotfiles.sh
@@ -4,19 +4,13 @@ echo $'Bootstrapping dotfiles...\n'
 
 # Symlink dotfiles to appropriate directories
 # TODO: Maybe redo vim symlink
-ln -sv vim/vimrc ~/.vimrc
+ln -sv vim/.vimrc ~
 ln -sv vim ~/.vim
 ln -sv zsh/.zshrc ~
 cp zsh/.hushlogin ~
-ln -sv vscode/settings.json ~/Library/Application\ Support/Code/User
+ln -sv git/.gitconfig ~
 cp xcode/Custom\ Default\ (Dark).xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes
 # TODO: Make script out of Xcode plist using `defaults` API
 # cp xcode/com.apple.dt.Xcode.plist ~/Library/Perferences
-
-# Install VSCode extensions
-for line in $(cat vscode/extensions)
-do
-    code --install-extension $line
-done
 
 echo $'Configure iTerm 2 to read our preferences by going to Preferences > General > Preferences > Load preferences...\n'
