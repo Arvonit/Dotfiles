b18ea5866bb5b3091de8cb22fff38f446ce44b4b
Arvind <me@arvind.dev>
2021-07-21 00:45:27 -0400

    Clean up and tweak fish config

--- fish/config-path.fish
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env fish
-# Setup variables in fish
-set -Up fish_user_paths ~/.cargo/bin
--- fish/config.fish
+++ fish/config.fish
@@ -4,8 +4,6 @@ abbr --add g git
 abbr --add l ls
 abbr --add p python3
 abbr --add v nvim
-# I rarely use this command and it conflicts with zoxide
-# abbr --add z 'zsh -c'
 abbr --add b 'bash -c'
 abbr --add t 'tmux attach || tmux new'
 abbr --add gst 'git status'
@@ -13,14 +11,14 @@ abbr --add gl 'git log --all --graph --decorate --oneline'
 
 # TODO: Add if here and clean up set commands with correct flags
 
-# Source zoxide
+# Enable zoxide
 zoxide init fish | source
 
 # Disable directory shortening in `prompt_pwd`
 set -g fish_prompt_pwd_dir_length 0
 
 # Disable greeting at startup
-set fish_greeting
+set -g fish_greeting
 
 # Set editor to Neovim
 set -gx EDITOR nvim
@@ -29,27 +27,34 @@ set -gx EDITOR nvim
 # This is native to Linux, but some programs support it on macOS
 set -gx XDG_CONFIG_HOME ~/.config
 
+# Disable less history 
+set -gx LESSHISTFILE /dev/null
+
+# Add directories to PATH
+fish_add_path ~/.local/bin
+
+# TODO: See if these universal variables can be converted into global variables
 # Custom syntax highlighting colors
-# - White for commands
-# - Yellow for subcommands
-# - Blue as accent color
+# - Yellow for commands
+# - White for subcommands
+# - Blue for operators
 # - Green for quotes
 # - Red for errors
 set -U fish_color_autosuggestion brblack
-set -U fish_color_command brwhite
+set -U fish_color_command yellow
 set -U fish_color_comment brblack
 set -U fish_color_end blue # ; &&
 set -U fish_color_error red
 set -U fish_color_escape blue # \n
-set -U fish_color_operator yellow # () $i
-set -U fish_color_param yellow
+set -U fish_color_operator brwhite # () $i
+set -U fish_color_param brwhite
 set -U fish_color_quote green
 set -U fish_color_redirection blue # > < 2>
 
 # user@hostname pwd $ ...
 function fish_prompt
-    # set_color normal
-    # echo -n '['(date +"%I:%M %p")']'
+    # set_color brblack
+    # echo -n '['(date +"%I:%M %p")'] '
     set_color yellow
     echo -n $USER
     set_color normal
@@ -67,9 +72,9 @@ end
 function fish_right_prompt
     # Save the return status of the previous command(s)
     set -l last_status $pipestatus
-    # Create a formatted string with return status
+    # Create a fancy string with said return status
     set -l status_string (__fish_print_pipestatus "[" "]" "|" (set_color $fish_color_status) \
         (set_color --bold $fish_color_status) $last_status)
-    # Display it
+    # Print it
     echo -n $status_string
 end
